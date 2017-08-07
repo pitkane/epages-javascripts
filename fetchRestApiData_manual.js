dd25596628c25d76266b980e045fc6448be44819
pitkane <mikko@mbit.fi>
2017-08-07 13:24:37 +0300

    Mods

--- fetchRestApiData_manual.js
+++ fetchRestApiData_manual.js
@@ -1,8 +1,9 @@
 /*
-
+1. Script parses REST API url from the current web page
+2. Parses product id from the page, and uses that to fetch data from ePages REST API
+3. Mounts data to Product page
 
 <script src="/WebRoot/Store/Shops/DemoShop/MediaGallery/fetchRestApiData_manual.js"></script>
-
 
 <script type="text/javascript">
   DUMP THE SCRIPT BELOW
@@ -12,72 +13,67 @@
 
 jQuery(document).ready(function($) {
   require(["jquery", "jquery/cookie", "ep/fn/busy"], function($) {
-    function fetchProductData() {
-      // resolve needed variables
-      console.log(
-        "requestProtocolAndServer: ",
-        window.ep.config.requestProtocolAndServer
-      );
-      console.log("storeFrontUrl: ", window.ep.config.storeFrontUrl);
+    fetchProductDataFromRest();
+  });
+});
 
-      var url =
-        window.ep.config.requestProtocolAndServer ||
-        "http://epages02.mikko.pri/";
-      var storefront =
-        window.ep.config.requestProtocolAndServer ||
-        "http://epages02.mikko.pri/epages/DemoShop.sf";
+function fetchProductDataFromRest() {
+  // resolve needed variables
+  console.log(
+    "requestProtocolAndServer: ",
+    window.ep.config.requestProtocolAndServer
+  );
+  console.log("storeFrontUrl: ", window.ep.config.storeFrontUrl);
 
-      // extremely shady
-      var shopName = storefront.substring(
-        storefront.lastIndexOf("/") + 1,
-        storefront.lastIndexOf(".")
-      );
+  var url =
+    window.ep.config.requestProtocolAndServer || "http://epages02.mikko.pri/";
+  var storefront =
+    window.ep.config.requestProtocolAndServer ||
+    "http://epages02.mikko.pri/epages/DemoShop.sf";
 
-      var restUrl = url + "rs/shops/" + shopName;
-      console.log(restUrl);
+  // extremely shady
+  var shopName = storefront.substring(
+    storefront.lastIndexOf("/") + 1,
+    storefront.lastIndexOf(".")
+  );
 
-      var canonicalUrl = window.ep.config.canonicalUrl;
-      var productId = /[^/]*$/.exec(canonicalUrl)[0];
+  var restUrl = url + "rs/shops/" + shopName;
+  console.log(restUrl);
 
-      console.log(productId);
+  var canonicalUrl = window.ep.config.canonicalUrl;
+  var productId = /[^/]*$/.exec(canonicalUrl)[0];
 
-      // http: //epages02.mikko.pri/rs/shops/DemoShop/products/?resultsPerPage=100&q=md_49417110
-      var requestUrl = restUrl + "/products/?q=" + productId;
+  console.log(productId);
 
-      console.log("requestUrl", requestUrl);
+  // http: //epages02.mikko.pri/rs/shops/DemoShop/products/?resultsPerPage=100&q=md_49417110
+  var requestUrl = restUrl + "/products/?q=" + productId;
+
+  console.log("requestUrl", requestUrl);
+
+  jQuery.ajax({
+    url: requestUrl,
+    type: "GET",
+    success: function(data, textStatus, jqXHR) {
+      console.log(data);
+
+      var productUrl = data.items[0].links[0].href;
 
       jQuery.ajax({
-        url: requestUrl,
+        url: productUrl,
         type: "GET",
         success: function(data, textStatus, jqXHR) {
-          console.log(data);
-
-          const productUrl = data.items[0].links[0].href;
-
-          jQuery.ajax({
-            url: productUrl,
-            type: "GET",
-            success: function(data, textStatus, jqXHR) {
-              console.log("Manufacturer", data.manufacturer);
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-              console.log("nounou");
-            }
-          });
+          console.log("Manufacturer", data.manufacturer);
         },
         error: function(jqXHR, textStatus, errorThrown) {
           console.log("nounou");
         }
       });
-
-      // "https://www.maalinpaikka.fi/epages/maalinpaikka.sf"
-      // http://epages02.mikko.pri/rs/shops/DemoShop
-
-      // jQuery(function () {
-      //   jQuery('.InfoArea').load('?ObjectPath=#Shop.Path[url]/Categories/TermsAndConditions .TermsAndConditions');
-      // });
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log("nounou");
     }
-
-    fetchProductData();
   });
-});
+
+  // "https://www.maalinpaikka.fi/epages/maalinpaikka.sf"
+  // http://epages02.mikko.pri/rs/shops/DemoShop
+}
